2e3c4839d8cd966010e86d3c4090a0e97189635c
Inderjit Gill <email@indy.io>
2011-06-13 07:50:23 +0100

    added youtube script

---
+++ youtube.user.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name           YouTube
+// @author         Inderjit Gill ( http://indy.io | http://twitter.com/InderjitGill )
+// @namespace      indy.io
+// @version        0.0.1
+// @description    Removes the annoying recommendations panels from the front page
+// @include        http://www.youtube.com/*
+// ==/UserScript==
+
+function stylesheetNode(css) {
+  var style = document.createElement("style");
+  style.setAttribute("type","text/css");
+  style.innerHTML = css;
+  return style;
+}
+
+function addStyleSheet(css) {
+  var head = document.getElementsByTagName("head")[0];
+  var style = stylesheetNode(css);
+
+  head.appendChild(style);
+}
+
+function removeRecommendedCrap() {
+  var parent = document.getElementById("hp-feed-items-container");
+
+  var gv = [];
+  for(var c=parent.firstChild; c!=null; c=c.nextSibling) {
+    var cl = c.classList;
+    if(cl && cl.length == 1 && cl[0] === 'grid-view') {
+      gv.push(c);
+    }
+  }
+
+  for(var i=0;i<gv.length;i++) {
+    parent.removeChild(gv[i]);
+  }
+}
+
+var _ran;
+function init() {
+	if(_ran) return;
+	_ran=true;
+
+	addStyleSheet("#homepage-side-content { display: none;}");
+  removeRecommendedCrap();
+}
+
+init();
